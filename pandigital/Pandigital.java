a0232a6f0f6108e6dd75e71a3b238142cec24f74
Vinson <55514029+vinsonchin@users.noreply.github.com>
2022-02-25 10:32:11 -0700

    Pandigital: misunderstood the prompt; tested only digits 1-9 and their multiples

--- pandigital/Pandigital.java
+++ pandigital/Pandigital.java
@@ -10,7 +10,7 @@ public class Pandigital {
 
         int largest = 0;
 
-        for(int i = 9; i > 0; i--) {
+        for(int i = 1; i < 9876; i++) {
 
             StringBuilder currentString = new StringBuilder();
             currentString.append(i);
@@ -22,9 +22,15 @@ public class Pandigital {
             }
 
             if(currentString.length() == 9) {
-                int currentValue = Integer.parseInt(String.valueOf(currentString));
-                if(currentValue > largest) {
-                    largest = currentValue;
+                String tempString = currentString.toString();
+                if(tempString.contains("1") && tempString.contains("2") && tempString.contains("3") &&
+                        tempString.contains("4") && tempString.contains("5") && tempString.contains("6") &&
+                        tempString.contains("7") && tempString.contains("8") && tempString.contains("9")) {
+
+                    int currentValue = Integer.parseInt(String.valueOf(currentString));
+                    if(currentValue > largest) {
+                        largest = currentValue;
+                    }
                 }
             }
         }
