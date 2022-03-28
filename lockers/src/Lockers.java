cca302fd0075f5e99d2d1b716a64a9a82948dbbd
Vinson <55514029+vinsonchin@users.noreply.github.com>
2022-03-28 13:16:32 -0600

    Number of open lockers

---
+++ lockers/src/Lockers.java
@@ -0,0 +1,24 @@
+/*  100 Lockers: There are 100 closed lockers in a hallway. A man begins by opening all 100 lockers.
+    Next, he closes every second locker. Then, on his third pass, he toggles every third locker
+    (closes it if it is open or opens it if it is closed). This process continues for 100 passes,
+    such that on each pass 'i', the man toggles every 'i'th locker. After his 100th pass in the hallway,
+    in which he toggles only locker #100, how many lockers are open?
+ */
+public class Lockers {
+
+    public static void main(String args[]) {
+        System.out.println("Number of open lockers for 100 lockers: " + openLockers(100));
+    }
+
+    /*  The number of times a locker is opened depends on the number of factors of its locker number.
+        If a locker number has an even number of factors, the locker will be closed at the end.
+        Every factor has a corresponding factor which creates a pair (e.g. factors of 6 are (1,6) and (2,3)).
+        This means all numbers except for perfect squares will have an even number of factors (locker will be closed).
+        We just need to count the number of perfect squares which happens to equal the square root of the number of
+        lockers without the decimal.
+     */
+    public static int openLockers(int numOfLockers) {
+        return (int) Math.sqrt(numOfLockers);
+    }
+
+}
